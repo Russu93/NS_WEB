d1ed1849c28c23dd047dd83a46514a25f3a86a85
Russu <artemrusu93@gmail.com>
2019-07-26 23:20:45 +0300

    0.4

--- js/vk.js
+++ js/vk.js
@@ -2,12 +2,6 @@
 var hrAuth = new XMLHttpRequest();
 var authLink = "https://oauth.vk.com/authorize?client_id=6796225&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52"
 
-hrAuth.open('GET',authLink,false);
+hrAuth.open('GET',authLink,true);
 
 hrAuth.send();
-
-if(hrAuth.status !=200){
-	alert("Ошибка! "+hrAuth.status+": "+hrAuth.statusText);
-}else{
-	alert(hrAuth.responseText);
-}
