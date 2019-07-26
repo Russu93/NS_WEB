512c2b9995a4363af6717dcc8fb57239c81812f8
Russu <artemrusu93@gmail.com>
2019-07-26 22:28:17 +0300

    0.2

---
+++ js/coreGame.js
@@ -0,0 +1,8 @@
+console.log("CoreGame подключен!")
+
+//canvas
+var canvas = document.getElementById("canvasGame");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = 'green';
+ctx.fillRect(10,10,100,100);
