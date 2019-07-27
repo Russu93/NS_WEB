beb8aa9da50c10eda93bf20beb064cd9ce23a9b8
Russu <artemrusu93@gmail.com>
2019-07-27 12:14:09 +0300

    Update coreGame.js

--- js/coreGame.js
+++ js/coreGame.js
@@ -1,4 +1,14 @@
 console.log("CoreGame подключен!")
+ VK.init(function() { 
+     // API initialization succeeded 
+     // Your code here
+     console.log("OK!")
+  }, function() { 
+     // API initialization failed 
+     // Can reload page here
+     console.log("NO OK!") 
+}, '5.101'); 
+
 
 //canvas
 var canvas = document.getElementById("canvasGame");
