71c1a3c941581ae5c49bb6b9b10c835e06ca7cdc
Russu <artemrusu93@gmail.com>
2019-07-27 13:40:53 +0300

    index up

--- js/vk.js
+++ js/vk.js
@@ -1,7 +1,14 @@
 
-var hrAuth = new XMLHttpRequest();
-var authLink = "https://oauth.vk.com/authorize?client_id=6796225&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52"
+<script>
+		window.onload = function() 
+		{
+    		VK.init(function() 
+    		{ 
+     			console.log("OK VK");
 
-hrAuth.open('GET',authLink,true);
+  			}, function(){ 
+     			console.log("NO VK");
 
-hrAuth.send();
+						}, '5.101'); 
+  		};
+  	</script>
